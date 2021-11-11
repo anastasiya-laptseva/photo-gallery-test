94013fd4904f84b62231fa2ffd4c072d0ff6aac7
Anastasiya Lapteva <anas.alex.novikova@gmail.com>
2021-11-11 11:33:21 +0300

    init project

---
+++ photo-gallery-testTests/photo_gallery_testTests.swift
@@ -0,0 +1,33 @@
+//
+//  photo_gallery_testTests.swift
+//  photo-gallery-testTests
+//
+//  Created by Anastasiya Laptseva on 11.11.21.
+//
+
+import XCTest
+@testable import photo_gallery_test
+
+class photo_gallery_testTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

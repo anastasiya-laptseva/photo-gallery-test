223f06ed0102fcd1c72dd5a2e0998aa96ec35a67
Anastasiya Lapteva <anas.alex.novikova@gmail.com>
2021-11-11 13:40:22 +0300

    UI

---
+++ photo-gallery-test/Slide.swift
@@ -0,0 +1,19 @@
+//
+//  Slide.swift
+//  photo-gallery-test
+//
+//  Created by Anastasiya Laptseva on 11.11.21.
+//
+
+import UIKit
+
+class Slide: UIView {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userUrlButton: UIButton!
+    @IBOutlet weak var photoUrlButton: UIButton!
+    
+    
+}
--- photo-gallery-test/ViewController.swift
+++ photo-gallery-test/ViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

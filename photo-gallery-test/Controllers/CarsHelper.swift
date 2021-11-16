67440a5a7f864e28e6ccc5d1bbf242a4837d5431
Anastasiya Lapteva <anas.alex.novikova@gmail.com>
2021-11-16 23:08:53 +0300

    attemp page controller

---
+++ photo-gallery-test/Controllers/CarsHelper.swift
@@ -0,0 +1,13 @@
+//
+//  CarsHelper.swift
+//  photo-gallery-test
+//
+//  Created by Anastasiya Laptseva on 16.11.21.
+//
+
+import UIKit
+
+struct PhotoHelper {
+    var name = String()
+    var image = UIImage()
+}
---
+++ photo-gallery-test/Controllers/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  photo-gallery-test
+//
+//  Created by Anastasiya Laptseva on 16.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+}
